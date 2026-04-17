788f0ee0d0cca7f277a8f8fcad6b310a5e63d915
John Alejandro <thejohn55555@gmail.com>
2026-04-17 13:02:25 +0200

    .

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using BaseCharacters;
 using Managers;
 using UnityEngine;
@@ -7,16 +8,35 @@ namespace Player
     public class PlayerHealth : CharacterHealth
     {
         protected PlayerMain playerMain;
+        private bool isCrounching;
+        private float hpHeal = 1f;
+        private float timeWaitHeal=2f;
+
         protected override void Awake()
         {
             base.Awake();
             playerMain = transform.root.GetComponent<PlayerMain>();
         }
 
+        public void Heal(float health)
+        {
+            Damageable(-health);
+        }
+        
         public override void Damageable(float damage)
         {
             base.Damageable(damage);
             EventManager.Instance.PlayerDamage(Health/MaxHealth,playerMain.PlayerID );
+        }
+
+        public IEnumerator HealRoutine()
+        {
+            while (isCrounching)
+            {
+                yield return new WaitForSeconds(timeWaitHeal);
+                if (!isCrounching) continue;
+                Heal(hpHeal);
+            }
         }
     }
 }
